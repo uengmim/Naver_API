4179e06e210d6f6334ba13bf20296cac41e1c913
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-06-02 14:07:34 +0900

    Rest API

---
+++ restaurant/src/test/java/com/example/restaurant/naver/NaverClientTest.java
@@ -0,0 +1,24 @@
+package com.example.restaurant.naver;
+
+import com.example.restaurant.naver.dto.SearchLocalReq;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class NaverClientTest {
+
+    @Autowired
+    private NaverClient naverClient;
+
+    @Test
+    public void localSearchTest(){
+
+        var search = new SearchLocalReq();
+        search.setQuery("갈비집");
+
+        var result  = naverClient.localSearch(search);
+        System.out.println(result);
+    }
+}
