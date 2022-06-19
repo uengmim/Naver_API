3faaf0de98610f62960f6f4661385a7c783826b0
uengmim <72143238+uengmim@users.noreply.github.com>
2022-06-19 23:27:20 +0900

    Update MemoryDbRepositoryIfs.java

--- restaurant/src/main/java/com/example/restaurant/db/MemoryDbRepositoryIfs.java
+++ restaurant/src/main/java/com/example/restaurant/db/MemoryDbRepositoryIfs.java
@@ -9,3 +9,22 @@ public interface MemoryDbRepositoryIfs<T> {
     void deleteById(int index);
     List<T> listAll();
 }
+    @Override
+    public T save(T entity) {
+        var optionalEntity = db.stream().filter(it -> it.getIndex() == entity.getIndex()).findFirst();
+
+        if (optionalEntity.isEmpty()) {
+            // db에 이미 데이터가 없는 경우
+            index++;
+            entity.setIndex(index);
+            db.add(entity);
+            return entity;
+        } else {
+            // db에 이미 데이터가 없는 경우
+            var preIndex = optionalEntity.get().getIndex();
+            entity.setIndex(preIndex);
+            deleteById(preIndex);
+            db.add(entity);
+            return entity;
+        }
+    }
